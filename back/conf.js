6d642906941c901ca5542a10c3ae4679fe131d9b
Hélèna Thomassin <thomassin.helena@gmail.com>
2020-06-17 16:17:36 +0200

    add routes get, post, put, delete

--- back/conf.js
+++ back/conf.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root', // à copmléter
-  password: 'Jaimemesenfants2@', // à compléter
+  password: '', // à compléter
   database: 'animals_walk',
 });
 
